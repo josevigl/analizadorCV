8ec356f4b6715921f5b6ea2bfe733cbf2151d069
JOSÉ VICENTE GARCÍA LÓPEZ <JoseVicente.Garcia6@alu.uclm.es>
2025-01-11 17:11:06 +0100

    Añadir plantilla SAM y la carpeta principal del proyecto

---
+++ src/app.py
@@ -0,0 +1,11 @@
+﻿def lambda_handler(event, context):
+    pass
+    # TODO: Función principal
+
+def evaluate_cv():
+    pass
+    # TODO: Función que pone la nota al CV
+
+def send_notification():
+    pass
+    # TODO: Función para enviar los msg
